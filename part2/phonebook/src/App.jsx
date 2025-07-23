dda1e0ce33d3103f2dcdf74558028b807a069969
tokisuno <lucaswmei@gmail.com>
2025-07-23 07:57:12 -0400

    halfway done phonebook assignment

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    if (newName === '') {
+      alert(`Missing name string, try again!`);
+      return;
+    }
+
+    const found = persons.find(({ name }) => name === newName) ? true : false;
+
+    if (found) {
+      alert(`${newName} already exists in phonebook!!`)
+      setNewName('');
+      setNewNumber('');
+    } else {
+      setPersons(persons.concat({
+        name: newName,
+        number: newNumber,
+        id: persons[persons.length - 1].id + 1
+      }));
+      setNewName('');
+      setNewNumber('');
+    }
+  }
+
+  const handleNameInput = (e) => setNewName(e.target.value);
+  const handleNumberInput = (e) => setNewNumber(e.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          <div>name: <input value={newName} onChange={handleNameInput} /></div>
+          <div>number: <input value={newNumber} onChange={handleNumberInput} /></div>
+          <button type="submit">add</button>
+        </div>
+      </form >
+      <h2>Numbers</h2>
+      <div>
+        {
+          persons.map(person => {
+            return (
+              <p key={person.name}>{person.name} {person.number}</p>
+            )
+          })
+        }
+      </div>
+
+    </div >
+  )
+}
+
+export default App
