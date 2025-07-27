32c3dc50680c74d2656fc4ed65fdd45596d09427
tokisuno <lucaswmei@gmail.com>
2025-07-27 17:20:30 -0400

    war crimes have been committed on this reop

---
+++ part2/dataforcountries/src/components/Results.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+const Results = ({ result, countries, setCountries }) => {
+  useEffect(() => {
+    if (countries) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then(res => {
+          res.data.forEach(c => {
+            const name = c.name.common.toLowerCase();
+            if (name.includes(result.toLowerCase())) {
+              setCountries(prev => [...prev, c.name.common]);
+            }
+          })
+        })
+        .catch(err => {
+          setCountries([]);
+        })
+    }
+  }, [result])
+
+  console.log(countries);
+
+  if (result === '') {
+    return (
+      <p>Please make a search :)</p>
+    );
+  }
+
+  if (countries.length > 9) {
+    return (
+      <p>Oops! Your search is too broad. Try to narrow it down.</p>
+    )
+  } else {
+    return (
+      <ul>
+        {countries.map(country => {
+          return (
+            <li key={country}>{country}</li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
+
+export default Results
