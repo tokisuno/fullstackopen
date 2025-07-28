7956429ca6a9bcc67fedf0061a606bf279f1314e
tokisuno <lucaswmei@gmail.com>
2025-07-28 14:06:43 -0400

    following along backend stuff

---
+++ following-along/backend/index.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express()
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JS",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world!</h1>');
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = notes.find(n => n.id === id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.statusMessage = "Current note does not exist";
+    res.status(404).end();
+  }
+})
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+
+  return String(maxId + 1);
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter(note => note.id !== id);
+  res.status(204).end();
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
