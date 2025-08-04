b5e325fd40871c422807244423d51f65aeb87462
tokisuno <lucaswmei@gmail.com>
2025-08-04 10:45:14 -0400

    finished part 4

--- part4/bloglist/backend/controllers/blog.js
+++ part4/bloglist/backend/controllers/blog.js
@@ -46,4 +46,33 @@ blogsRouter.post('/', (req, res, next) => {
     .catch(error => next(error))
 })
 
+blogsRouter.delete('/:id', (req, res) => {
+  Blog.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (req, res, next) => {
+  const { title, author, url, likes } = req.body;
+
+  Blog.findById(req.params.id)
+    .then((blog) => {
+      if (!blog) {
+        return res.status(404).end()
+      }
+      blog.title = title
+      blog.author = author
+      blog.url = url
+      blog.likes = likes
+
+      return blog.save().then((updatedBlog) => {
+        res.json(updatedBlog)
+      })
+    })
+    .catch(error => next(error))
+
+})
+
 module.exports = blogsRouter
