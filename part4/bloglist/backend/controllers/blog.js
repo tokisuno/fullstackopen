f70fd57fe9b2c784e77144c4afe44b77208f139b
tokisuno <lucaswmei@gmail.com>
2025-08-15 16:28:02 -0400

    requests and whatnot

--- part4/bloglist/backend/controllers/blog.js
+++ part4/bloglist/backend/controllers/blog.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
@@ -19,12 +20,26 @@ blogsRouter.get('/:id', async (request, response, next) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
-  console.log(request.body);
-  // const user = await User.findById(body.userId);
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response(401).json({
+      error: 'Token invalid'
+    })
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
     return response.status(400).json({
--- part4/bloglist/backend/index.js
+++ part4/bloglist/backend/index.js
@@ -6,12 +6,6 @@ app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
 
-// what to put in here
-// - [x] ./utils/config
-// - [x] ./utils/logger
-// - [ ] ./utils/middleware?
-// - [x] ./controllers/blogs
-
 app.get('/api/blogs', (req, res) => {
   Blog.find({}).then((blogs) => {
     res.json(blogs)
