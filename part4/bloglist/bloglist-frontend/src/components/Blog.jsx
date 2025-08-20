ad3b19b16ee1c4e6babf4cac7efda80188ec9dff
tokisuno <lucaswmei@gmail.com>
2025-08-20 12:59:20 -0400

    exercise 5.7 (phew)

--- part4/bloglist/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author} (<a href={blog.url}>LINK</a>)
-  </div>
-)
+import { useState, useImperativeHandle, forwardRef } from 'react';
+
+const Blog = forwardRef((props, ref) => {
+  const [expand, setExpand] = useState(false);
+
+  const hideWhenVisible = { display: expand ? "none" : "" };
+  const showWhenVisible = { display: expand ? "" : "none" };
+
+  const toggleExpand = () => {
+    setExpand(!expand);
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleExpand
+    }
+  })
+
+  const blogStyle = {
+    border: 'solid',
+  }
+
+  console.log(props)
+  return (
+    <div style={blogStyle}>
+      <div>
+        {props.blog.title}
+        <div style={hideWhenVisible}>
+          <button onClick={toggleExpand}>Expand</button>
+        </div>
+        <div style={showWhenVisible}>
+          <button onClick={toggleExpand}>Close</button>
+          <ul>
+            <li>Written by: {props.blog.author}</li>
+            <li>Likes: {props.blog.likes} <button>Like!</button></li>
+            <li><a href={props.blog.url}>LINK</a></li>
+            <li>{props.blog.user.username}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+})
 
 export default Blog
